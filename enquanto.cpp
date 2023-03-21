82bd08030a7d9d2a9976de38c577397f65cb35c9
thecookiebutter <127798899+thecookiebutter@users.noreply.github.com>
2023-03-20 22:11:27 -0300

    Add files via upload

---
+++ enquanto.cpp
@@ -0,0 +1,17 @@
+// Funcoes importadas
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    // Variavel
+    int num = 1;
+    // Roda o codigo enquanto a variavel e menor que dez
+    while(num<10){
+        //Imprimindo a variavel para o usuario
+        cout << "Numero: "<<num<<endl;
+        // Aumentando a variavel em um
+        num++;
+    }
+    return 0;
+}
