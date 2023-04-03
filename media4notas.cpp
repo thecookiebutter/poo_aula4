8fbefe93e76b5ca57712ce00e4b8d9e316b273c5
thecookiebutter <127798899+thecookiebutter@users.noreply.github.com>
2023-04-03 20:16:26 -0300

    Update media4notas.cpp

--- media4notas.cpp
+++ media4notas.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
+    // Variaveis
     double nota1, nota2, nota3, nota4;
+    string continuar = "S";
+    string nome;
+    int ra;
+    // Programa roda o codigo enquando o usuario quiser baseado em um string
+    do{
+    // Pedindo input do usuario para identificar o aluno
+    cout << "Digite o nome do aluno: " << endl;
+    // Registrando input do usuario
+    cin >> nome;
+    // Pedindo input do usuario para identificar o aluno
+    cout << "Digite o RA do aluno: " << endl;
+    // Registrando input do usuario
+    cin >> ra;
     // Pedindo input do usuario para primeira nota
     cout << "Digite a sua primeira nota do semestre: " << endl;
     // Registrando input do usuario
@@ -26,11 +40,18 @@ int main() {
     // Mostrando resultado da media para o usuario
     cout << "A sua media no semestre foi: " << soma << endl << endl;
     // Mostrando resultado de aprovacao ou reprovacao para o usuario
-    if(soma>=7){
-        cout << "Parabens, voce foi aprovado!" << endl;
-    }
-    else{cout << "Eita, voce foi reprovado!" << endl;
-    };
+    switch((int)soma){
+    case 10: 
+    case 7: cout << "Parabens, voce foi aprovado!" << endl;
+    break;
+    case 6:
+    case 5: cout << "Voce vai para a recuperacao!" << endl;
+    break;
+    default: cout << "Infelizmente voce foi reprovado!" << endl;
+    };// Informa ao usuario que pode continuar, mudando a string
+    cout << "Quer continuar? Digite S" << endl;
+    cin >> continuar;
+    } while(continuar == "S" || continuar ==  "s");
     
     return 0;
 }
